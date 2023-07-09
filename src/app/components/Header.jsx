7a07f8b5ae134afc80e9cc45e498385e85b4a5c7
shuvo9620 <smahfuzur0@gmail.com>
2023-07-09 15:09:45 +0600

    code added in global css

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styles from '@/app/styles/navbar.module.css'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Header = () => {
     return (
-        <header>
-            <div className=''>
-
+        <header className={styles.main_header}>
+            <div className={styles.navbar_brand}>
+                <Link href="/">
+                <Image src="/logo2.webp" alt='logo' width="150" height="40"/>
+                </Link>
+            </div>
+            <div className={styles.navbarList}>
+                ggg
+                aaaaaaaaaa
+                ggggggg
+                aaaaaa
             </div>
         </header>
     );
