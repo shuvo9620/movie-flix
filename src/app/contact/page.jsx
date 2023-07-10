98af53675873515894fdbe1c48d7f956e536bc50
shuvo9620 <smahfuzur0@gmail.com>
2023-07-10 11:27:55 +0600

    Contact from added in contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ContactCard from '../components/ContactCard';
+import styles from '@/app/styles/contact.module.css'
+import ContactForm from '../components/ContactForm';
 
 const Contact = () => {
     return (
         <>
-        <h1>Contact Us</h1>
-        <ContactCard></ContactCard>
+            <div className={styles.container}>
+                <h1>Contact Us</h1>
+                <ContactCard></ContactCard>
+
+                <section className={styles.contact_section}>
+                <h2>We'd love to hear <span> from you </span></h2>
+                <ContactForm></ContactForm>
+
+                </section>
+            </div>
+
         </>
     );
 };
